49e543dde300b71fa49aa4e934d291b50a507275
Abraham Murillo <abraham140600@gmail.com>
2021-01-08 12:59:36 -0600

    Point, Segment, Circle working fine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from "react-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Canvas from "./Canvas";
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      objects: []
+    }
+
+    this.getInput = this.getInput.bind(this)
+  }
+
+  getInput(event) {
+    this.setState(() => {
+      const text = event.target.value
+      const newObjects = text.split('\n').map((str) => {
+        return str.split(' ')
+      })
+
+      return {
+        objects: newObjects
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Geometry noob version</h3>
+
+        <div className="container">
+          <textarea
+            type="text"
+            className="input"
+            onChange={this.getInput}
+            >
+          </textarea>
+
+          <Canvas objects={this.state.objects} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
